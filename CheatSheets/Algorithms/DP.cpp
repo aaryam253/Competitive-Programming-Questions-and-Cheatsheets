66313497fb849ac87b055f738dc24f70496c2b02
lws803 <lws803@gmail.com>
2019-01-13 00:35:06 +0800

    Clarified some descriptions

--- CheatSheets/Algorithms/DP.cpp
+++ CheatSheets/Algorithms/DP.cpp
@@ -35,7 +35,7 @@ int recurse (int i, int j) {
     }
 
     int ans = -1;
-    // We've stored the number of states in arr[i][0], last column of that row
+    // We've stored the total number of states in arr[i][0], last column of that row
     for (int state = 1; state <= arr[i][0]; state++) {
         ans = max(ans, recurse(i+1, j - arr[i][state])); 
         // Choose an optimal ans, after exploring all the possibilities of the next state
@@ -72,7 +72,7 @@ int main () {
     int money;
     for (int g = 1; g < C; g++) {
         for (money = 0; money < M; money++) {
-            // We've stored the number of states in price[i][0], last column of that row
+            // We've stored the total number of states in price[i][0], last column of that row
             for (int k = 1; k <= price[g][0]; k++) {
                 if (money - price[g][k] >= 0)
                     reachable[g][money - price[g][k]] = true; // Visited and reachable
